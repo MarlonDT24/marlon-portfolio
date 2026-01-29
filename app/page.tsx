2bd17e78b15476b502065b519344fbd272add419
MarlonDT24 <torresmarlon40@gmail.com>
2026-01-29 16:40:53 +0100

    fixed: bad gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import CoverParticles from "@/components/cover-particles";
 import Introduction from "@/components/introduction";
 import TransitionPage from "@/components/transition-page";
 
@@ -6,8 +5,7 @@ export default function Home() {
   return (
     <main>
       <TransitionPage />
-      <div className="flex min-h-screen h-full bg-no-repeat bg-gradient-cover">
-        
+      <div className="flex min-h-screen h-full bg-no-repeat">
         <Introduction />
       </div>
     </main>
